5e9c9dd5aa9a3dd3379a5cef953ea29db05f0be4
gsw07065 <gsw07065@naver.com>
2024-10-02 17:39:44 +0900

    first commit

--- js/javacsript.js
+++ js/javacsript.js
@@ -1,6 +1,20 @@
 let s4conLi = document.querySelectorAll(".contents li");
 let s4contxtLi = document.querySelectorAll(".con_txt li");
 
+//1280 밑 header
+let btn = document.querySelectorAll(".side .btn>li p");
+  let btnP = document.querySelectorAll(".side .btn>li");
+      
+      btn.forEach(function(v,k){
+        v.onclick = function(){
+          let bool4 = btnP[k].querySelector(".sub").classList.contains("on");
+          btnP[k].querySelector(".sub").classList.remove("on");
+          if(!bool4){
+            btnP[k].querySelector(".sub").classList.add("on");
+          }
+        }
+      });
+
 function my3(){
   s4conLi.forEach(function(v){
     v.classList.remove("on");
